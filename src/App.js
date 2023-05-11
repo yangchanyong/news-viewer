1a0d7bff2c9734ef8e38324ecdf9f9f3561f326f
ycy <cksdyd93@gmail.com>
2023-05-11 14:13:51 +0900

    23-05-11 4교시

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [data, setData] = useState();
+  const onClick = async () => {
+    // axios
+    // .get('https://jsonplaceholder.typicode.com/todos/1')
+    // .then((response) => {
+    //   setData(response.data);
+    // });
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos/1'
+    );
+    setData(response.data)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />
+      )}
     </div>
   );
 }
